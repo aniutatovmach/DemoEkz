5e74a6cb8d99f906c027cde729d259bc4666b630
aniuta <aniutatovmach@gmail.com>
2022-06-28 00:22:21 +0300

    first commit

---
+++ Demo_TovmachAO/AdminInfo.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Demo_TovmachAO
+{
+    /// <summary>
+    /// Логика взаимодействия для AdminInfo.xaml
+    /// </summary>
+    public partial class AdminInfo : Window
+    {
+        public AdminInfo()
+        {
+            InitializeComponent();
+        }
+
+        private void OrdersButton_Click(object sender, RoutedEventArgs e)
+        {
+            Seller seller = new Seller(this);
+            this.Hide();
+            seller.Show();
+        }
+
+        private void ClientsButotn_Click(object sender, RoutedEventArgs e)
+        {
+            AddClientPage addClientPage = new AddClientPage();
+            addClientPage.Show();
+            this.Close();
+        }
+    }
+}
